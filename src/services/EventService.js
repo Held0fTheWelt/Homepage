9599493daa588ce54fc90d454ac3abd3c93b084f
Yves Tanas <yvta@dcc.ruhr>
2021-03-23 13:19:44 +0100

    Update EventService.js

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/Held0fTheWelt/YvesProjectDataMockup',
+    baseURL: 'https://my-json-server.typicode.com/Held0fTheWelt/Homepage',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
