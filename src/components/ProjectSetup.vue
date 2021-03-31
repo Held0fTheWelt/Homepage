323b4e3cdb405d072bcb40602efbc392eab7164e
Yves Tanas <yvta@dcc.ruhr>
2021-03-31 15:00:34 +0200

    Create ProjectSetup.vue

---
+++ src/components/ProjectSetup.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <v-file-input label="Select MenuImage">
+        <v-file-input label="Select Main Video-Resource">
+
+        </v-file-input>
+    </v-container>
+</template>
+
+<script>
+  export default {
+   
+  }
+</script>
+
+<style scoped>
+
+</style>
