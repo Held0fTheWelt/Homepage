32bd877449fefd6467cc1ac0fa8efa8d2bdb5a2d
Yves Tanas <yvta@dcc.ruhr>
2021-04-01 11:07:42 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <Navbar />
     <!-- <v-main> -->
       <v-main id="content">
-        <router-view></router-view>
+        <transition name="slide-fade" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </v-main>
     <!-- </v-main> -->
     <Footer />
@@ -36,4 +38,33 @@ export default {
 table.v-table tbody td, table.v-table tbody th {
     height: 19px;
 } 
+
+/*** Fade Transitions */
+.slide-fade-enter{
+  transform: translate(10%);
+  opacity: 0;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.2s ease;
+}
+
+.slide-fade-leave-to{
+  transform: translate(-10%);
+  opacity: 0;
+}
+/*** Fade Transitions */
+.fade-enter{
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to{
+  opacity: 0;
+}
 </style>
