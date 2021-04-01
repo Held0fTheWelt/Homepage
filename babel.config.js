10716d3cb73d05f2a5999cd61a7e616399d61e17
Yves Tanas <yvta@dcc.ruhr>
2021-04-01 12:03:38 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+   build: {
+      extend(config, ctx) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+    }
+  }
 };
