708c7a762057bc944f0dd48539e087e5ad520939
Yves Tanas <yvta@dcc.ruhr>
2021-04-01 11:43:10 +0200

    Update Projects.vue

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -1,69 +1,69 @@
 <template>
-  <router-link class="projectLink" :to="{name: 'Project' , params: {id: project.id }}">
-    <v-card v-if="$vuetify.breakpoint.smAndDown" class="mx-auto project-xs">
-      <v-row>
-        <v-col cols="12"  class="hoverData">
-            <!-- Image goes here -->
-            <v-img v-bind:src="image" alt="Project Image"/>
-        </v-col>
-      </v-row>
-        <!-- Display Data-->
-        <v-row cols="12"  class="hoverData">
-            <v-col>
-                <h3 >@ {{project.location}} on {{project.date}}</h3> 
-            </v-col>
-        </v-row>
-        <v-row cols="12"  class="hoverData">
-            <v-col>
-                <h4>{{project.title}}</h4>                        
-            </v-col>
-        </v-row>
-        <v-row cols="12"  class="hoverData">
-            <v-col>
-                {{project.description}}
-            </v-col>
-        </v-row> 
-        <v-row cols="12" class="hoverData {'pad-xs' : $vuetify.breakpoint.xs}">
-            <v-col>
-                {{project.shortStory}}
-            </v-col>
-        </v-row> 
-    </v-card>
-    <v-card v-if="$vuetify.breakpoint.mdAndUp" class="mx-auto project-large">
-        <v-card-text>
+    <router-link class="projectLink" :to="{name: 'Project' , params: {id: project.id }}">
+      <v-card v-if="$vuetify.breakpoint.smAndDown" class="mx-auto project-xs">
         <v-row>
-            <v-col cols="7"  class="hoverData">
-                <!-- Image goes here -->
-                <v-img v-bind:src="image" alt="Project Image"/>
+          <v-col cols="12"  class="hoverData">
+              <!-- Image goes here -->
+              <v-img v-bind:src="image" alt="Project Image"/>
+          </v-col>
+        </v-row>
+          <!-- Display Data-->
+          <v-row cols="12"  class="hoverData">
+              <v-col>
+                  <h3 >@ {{project.location}} on {{project.date}}</h3> 
+              </v-col>
+          </v-row>
+          <v-row cols="12"  class="hoverData">
+              <v-col>
+                  <h4>{{project.title}}</h4>                        
+              </v-col>
+          </v-row>
+          <v-row cols="12"  class="hoverData">
+              <v-col>
+                  {{project.description}}
+              </v-col>
+          </v-row> 
+          <v-row cols="12" class="hoverData {'pad-xs' : $vuetify.breakpoint.xs}">
+              <v-col>
+                  {{project.shortStory}}
+              </v-col>
+          </v-row> 
+      </v-card>
+      <v-card v-if="$vuetify.breakpoint.mdAndUp" class="mx-auto project-large">
+          <v-card-text>
+          <v-row>
+              <v-col cols="7"  class="hoverData">
+                  <!-- Image goes here -->
+                  <v-img v-bind:src="image" alt="Project Image"/>
 
-            </v-col>
-            <v-col cols="5">
-                <!-- Display Data-->
-                <v-row  class="hoverData">
-                    <v-col>
-                        <h3>@ {{project.location}} on {{project.date}}</h3> 
-                    </v-col>
-                </v-row>
-                <v-row  class="hoverData">
-                    <v-col>
-                        <h4>{{project.title}}</h4>                        
-                    </v-col>
-                </v-row>
-                <v-row  class="hoverData">
-                    <v-col>
-                        {{project.description}}
-                    </v-col>
-                </v-row> 
-                <v-row class="hoverData">
-                    <v-col>
-                        {{project.shortStory}}
-                    </v-col>
-                </v-row>                
-            </v-col>
-        </v-row>     
-        </v-card-text>   
-    </v-card>
-  </router-link>
+              </v-col>
+              <v-col cols="5">
+                  <!-- Display Data-->
+                  <v-row  class="hoverData">
+                      <v-col>
+                          <h3>@ {{project.location}} on {{project.date}}</h3> 
+                      </v-col>
+                  </v-row>
+                  <v-row  class="hoverData">
+                      <v-col>
+                          <h4>{{project.title}}</h4>                        
+                      </v-col>
+                  </v-row>
+                  <v-row  class="hoverData">
+                      <v-col>
+                          {{project.description}}
+                      </v-col>
+                  </v-row> 
+                  <v-row class="hoverData">
+                      <v-col>
+                          {{project.shortStory}}
+                      </v-col>
+                  </v-row>                
+              </v-col>
+          </v-row>     
+          </v-card-text>   
+      </v-card>
+    </router-link>
 </template>
 
 <script>
