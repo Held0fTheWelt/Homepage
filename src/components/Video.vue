6a860da219bfed4c740618f220654546dc693f12
Yves Tanas <Yves.Tanas@gmx.de>
2021-04-06 14:39:27 +0200

    Update Video.vue

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,11 +1,36 @@
 <template>
-<div></div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <video autoplay controls>
+          <source v-bind:src="video" type="video/mp4" />
+          <!-- <source src="../assets/Logo.ogg" type="video/ogg"> -->
+          Ihr Browser kann dieses Video nicht wiedergeben.<br />
+          Dieser Film zeigt einen Trailer zum Projekt.
+        </video>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'VideoComponent',
+  props: {
+    msg: String,
+    required: true,
+  },
+  data() {
+    return {
+      video: require(this.msg),
+    }
+  },
+}
 </script>
 
 <style scoped>
-
+video {
+  object-fit: contain;
+  width: 100%;
+}
 </style>
