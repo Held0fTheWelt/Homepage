78aafa698c1b4c5caaff52dae908eb2e20591976
Yves Tanas <Yves.Tanas@gmx.de>
2021-04-09 19:27:24 +0200

    Update Specs.vue

--- src/components/projects/Specs.vue
+++ src/components/projects/Specs.vue
@@ -2,8 +2,28 @@
   <v-card>
     <v-card-title>
       <h1>Specifications</h1>
-    </v-card-title></v-card
-  >
+    </v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col cols="12">
+            <h3>Minimum Specifications:</h3>
+             </v-col>
+      </v-row>
+      
+      <v-row>
+        <v-col cols="12"><p>
+            Operating Sytem: <br/>
+            Processor: <br/>
+            Memory: <br/>
+            Graphice: <br/>
+            Storage: <br/>
+            Equipment: 
+            </p>
+        </v-col>
+      </v-row>
+      
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -14,5 +34,8 @@ export default {}
 h1 {
   text-align: center;
   width: 100%;
+}
+h3, p {
+    padding-left: 5%;
 }
 </style>
