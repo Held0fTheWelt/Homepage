992de2e95dffb2a7ed5b5afaeae4b2148b9c2350
Yves Tanas <Yves.Tanas@gmx.de>
2021-04-12 16:52:46 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import vueApexCharts from 'vue-apexcharts'
-
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
-  vueApexCharts,
   render: (h) => h(App),
 }).$mount('#app')
+
+Vue.use(vueApexCharts)
+
+Vue.component('apexchart', vueApexCharts)
--- src/views/ChartComponent.vue
+++ src/views/ChartComponent.vue
@@ -1,34 +1,41 @@
 <template>
-   <div>
-     <VueApexCharts width="500" type="bar" :options="chartOptions" :series="series"></VueApexCharts>
-   </div>
+  <div>
+  <apexchart width="500"  :options="options" :series="series"></apexchart>
+  </div>
 </template>
 
 <script>
-    // <ApexCharts/>
-// import ApexCharts from 'apexcharts'
-import VueApexCharts from 'vue-apexcharts'
+// <ApexCharts/>
 export default {
-  components: {
-    VueApexCharts,
-  },
   data: function() {
-      return {
-        chartOptions: {
-          chart: {
-            id: 'vuechart-example'
-          },
-          xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-          }
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example',
+          type: 'pie'
         },
-        series: [{
-          name: 'series-1',
-          data: [30, 40, 35, 50, 49, 60, 70, 91]
-        }]
-      }
-    },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        },
+        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+            responsive: [{
+              breakpoint: 480,
+              options: {
+                chart: {
+                  width: 200
+                },
+                legend: {
+                  position: 'bottom'
+                }
+              }
+            }]
+      },
+  series: [44, 55, 13, 43, 22],
+    }
+  }
 }
 </script>
+<style scoped>
 
-<style scoped></style>
+
+</style>
