df0c724c0fb3c623b0bddebc4cd80dad20c748df
Yves Tanas <Yves.Tanas@gmx.de>
2021-04-12 14:54:14 +0200

    Update

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+// webpack.config.js
+module.exports = {
+    // ...
+    vue: {
+      html: {
+        // all loader queries can be specified here
+        // also, you can specify options for htmlMinifier here.
+      }
+    }
+  }
