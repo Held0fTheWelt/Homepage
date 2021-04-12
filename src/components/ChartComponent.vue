e7a3ffb4bed9b0dee871b879106b5bedc6ce6cbf
Yves Tanas <Yves.Tanas@gmx.de>
2021-04-12 15:55:59 +0200

    Update

---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ApexCharts/>
+
+  </div>
+</template>
+
+<script>
+import ApexCharts from 'apexcharts'
+
+export default {
+  components: {
+    ApexCharts,
+  },
+
+}
+</script>
+
+<style scoped></style>
