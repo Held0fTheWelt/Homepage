c2cb452ff450596693061cc42d3d6bb8fe1edbed
Yves Tanas <Yves.Tanas@gmx.de>
2021-04-16 20:00:56 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 // webpack.config.js
 module.exports = {
   // ...
-  vue:  {
-    test: /\.html$/,
-    exclude: /node_modules/,
-    use: [
-        {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
             loader: "html-loader",
             options: {
-                attributes: {
-                    list: [
-                        {
-                            tag: "source",
-                            attribute: "src",
-                            type: "src"
-                        }
-                    ]
-                }
+              attributes: {
+                list: [
+                  {
+                    tag: "source",
+                    attribute: "src",
+                    type: "src"
+                  }
+                ]
+              }
             }
-        }
-    ]
-}
+          }
+        ]
+      },
+    ],
+  },
+  vue: {
+
+  }
 }
