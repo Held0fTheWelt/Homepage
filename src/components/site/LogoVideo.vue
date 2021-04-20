22883e06c467e554ffb954d593a77839af99c772
Yves Tanas <Yves.Tanas@gmx.de>
2021-04-20 13:38:38 +0200

    Update LogoVideo.vue

--- src/components/site/LogoVideo.vue
+++ src/components/site/LogoVideo.vue
@@ -1,5 +1,5 @@
 <template>
-<iframe width="100%" height="100%" src="//www.youtube.com/embed/Y_kQ8DO3Wuw?autoplay=1&loop=1&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe>
+<iframe width="100%" height="100%" src="//www.youtube.com/embed/Y_kQ8DO3Wuw?loop=1&fs=0&playlist=Y_kQ8DO3Wuw&disablekb=1&controls=0&autoplay=1&iv_load_policy=3&modestbranding=1&origin=https://yves-tanas.onrender.com/&playsinline=1&showinfo=0" frameborder="0" allowfullscreen></iframe>
 </template>
 
 <script>
